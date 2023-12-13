be40abec8b32259f3e4256a3c2a0ec28f518afb5
Afolabi Damilare <afolabidamilare08@gmail.com>
2023-12-13 03:17:09 +0100

    pushDashboard

--- src/pages/dapp/dappDashboard.jsx
+++ src/pages/dapp/dappDashboard.jsx
@@ -1,4 +1,5 @@
 import DappHeader from "../../components/dappHeader";
+import WeiImg from '../../assets/wei.png';
 
 const DappDashboard = () => {
 
@@ -7,7 +8,104 @@ const DappDashboard = () => {
         <div className="dappDashboard" >
             <DappHeader/>
 
+            <div className="dappDashboard_body" >
 
+                {/* <div className="dappDashboard_nowallet" >
+
+                                <h4>Sign In</h4>
+
+                                <h6>Connect your wallet to see your positions</h6>
+
+                                <button>
+                                    Connect Wallet
+                                </button>
+
+                            </div> */}
+
+                    <div className="dappDashboard_colored" >
+                        Earn
+                    </div>
+
+                    {/* <div className="dappDashboard_empty" >
+
+                        <h6>You have no open  positions</h6>
+
+                        <button>
+                            Open New Position
+                        </button>
+
+                    </div> */}
+
+                    <div className="dappDashboard_table" >
+
+                        <div className="dappDashboard_table_top" >
+
+                            <div className="dappDashboard_table_top_value" >
+                                Value
+                            </div>
+
+                            <div className="dappDashboard_table_top_rate" >
+                                Rate
+                            </div>
+
+                            <div className="dappDashboard_table_top_balance" >
+                                Balance
+                            </div>
+
+                            <div className="dappDashboard_table_top_btn" >
+                                {/* Balance */}
+                            </div>
+
+                        </div>
+
+                        <div className="dappDashboard_table_row" >
+
+                            <div className="dappDashboard_table_row_value" >
+                                <img src={WeiImg} alt="Img" />
+                                <h5>Default Goerli USDC MetaMorpho</h5>
+                            </div>
+
+                            <div className="dappDashboard_table_row_rate" >
+                                <h5>0.00%</h5>
+                            </div>
+
+                            <div className="dappDashboard_table_row_balance" >
+                                <img src={WeiImg} alt="Img" />
+                                <h5>2.00 USDC</h5>
+                            </div>
+
+                            <div className="dappDashboard_table_row_btn" >
+
+                                <button className="dappDashboard_table_row_btn_supply" >Supply</button>
+                                <button className="dappDashboard_table_row_btn_with" >Withdraw</button>
+
+                            </div>
+
+                        </div>
+
+                        <div className="dappDashboard_table_btm" >
+
+                        </div>
+
+                    </div>
+
+                    <div className="dappDashboard_colored" >
+                        Borrow
+                    </div>
+
+                    <div className="dappDashboard_empty" >
+
+                        {/* <h4>Sign In</h4> */}
+
+                        <h6>You have no open  positions</h6>
+
+                        <button>
+                            Open New Position
+                        </button>
+
+                    </div>
+
+            </div>
 
         </div>
 
